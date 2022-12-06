dc9b4c2921b6c5684e2e2158c9e6a61ffc6171e0
andresrh11 <andresr10020@gmail.com>
2022-12-06 15:15:21 -0500

    edit save

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,7 +1,48 @@
 import Link from "next/link";
 import Layout from "../layout";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function People({ person, tasks }) {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+    completed: "",
+    startDate: "",
+    endDate: "",
+    id: "",
+    personId: "",
+  });
+
+  useEffect(() => {
+    putCompleted(task.id, task);
+  }, [task]);
+  const handleClick = (e) => {
+    let taskFind = tasks.filter((f) => f.id == e.target.value);
+
+    if (taskFind[0].completed === true) {
+      setTask({
+        title: taskFind[0].title,
+        description: taskFind[0].description,
+        completed: false,
+        startDate: taskFind[0].startDate,
+        endDate: taskFind[0].endDate,
+        id: taskFind[0].id,
+        personId: taskFind[0].personId,
+      });
+    } else {
+      setTask({
+        title: taskFind[0].title,
+        description: taskFind[0].description,
+        completed: true,
+        startDate: taskFind[0].startDate,
+        endDate: taskFind[0].endDate,
+        id: taskFind[0].id,
+        personId: taskFind[0].personId,
+      });
+    }
+    window.location.reload();
+  };
   return (
     <>
       <Layout>
@@ -24,7 +65,7 @@ export default function People({ person, tasks }) {
                 <Link href={`/tasks/${e.id}/edit`}>
                   <button>Edit task</button>
                 </Link>
-                <button>
+                <button value={e.id} onClick={handleClick}>
                   {e.completed ? "Mark as not completed" : "Mark as completed"}
                 </button>
               </div>
@@ -44,7 +85,7 @@ export default function People({ person, tasks }) {
 async function getAllPeopleId() {
   const data = await fetch("http://localhost:3001/people");
   const jsondata = await data.json();
-  
+
   return jsondata.map((e) => ({
     params: {
       id: e.id.toString(),
@@ -83,4 +124,13 @@ export async function getStaticProps({ params }) {
       tasks,
     },
   };
+}
+export async function putCompleted(id, payload) {
+  try {
+    console.log(payload);
+    const res = await axios.put(`http://localhost:3001/tasks/${id}`, payload);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- pages/tasks/[id]/edit.js
+++ pages/tasks/[id]/edit.js
@@ -1,37 +1,121 @@
 import Layout from "../../layout";
+import axios from "axios";
 
-export default function EditTask() {
+import { useState } from "react";
+export default function EditTask({ tasks }) {
+  console.log(tasks);
+  const [task, setTask] = useState({
+    id: tasks[0].id,
+    title: tasks[0].title,
+    description: tasks[0].description,
+    completed: tasks[0].completed,
+    startDate: tasks[0].startDate,
+    endDate: tasks[0].endDate,
+    personId: tasks[0].personId,
+  });
+
+  const handleInputChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    putTask(tasks[0].id, task);
+    setTask({
+      id: tasks[0].id,
+      title: tasks[0].title,
+      description: tasks[0].description,
+      completed: tasks[0].completed,
+      startDate: tasks[0].startDate,
+      endDate: tasks[0].endDate,
+      personId: tasks[0].personId,
+    });
+  };
   return (
     <>
       <Layout>
-        <form type="submit">
+        <form onSubmit={handleSubmit}>
           <label>
-            <h1>Full Name:</h1>
-            <input placeholder="type here" />
+            <h1>Title:</h1>
+            <input
+              placeholder={tasks[0].title}
+              name="title"
+              onChange={handleInputChange}
+            />
           </label>
           <label>
-            <h1>Age:</h1>
-            <input placeholder="type here" />
+            <h1>Description:</h1>
+            <input
+              placeholder={tasks[0].description}
+              name="description"
+              onChange={handleInputChange}
+            />
           </label>
           <label>
-            <h1>Occupation:</h1>
-            <input placeholder="type here" />
+            <h1>Status:</h1>
+
+            {tasks[0].completed ? (
+              <select name="completed" onChange={handleInputChange}>
+                <option value={true}>Completed</option>
+                <option value={false}> Not Completed</option>
+              </select>
+            ) : (
+              <select name="completed" onChange={handleInputChange}>
+                <option value={false}> Not Completed</option>
+                <option value={true}>Completed</option>
+              </select>
+            )}
           </label>
-          <label>
-            <h1>Nickname:</h1>
-            <input placeholder="type here" />
-          </label>
-          <label>
-            <h1>Gender:</h1>
-            <input placeholder="type here" />
-          </label>
-          <label>
-            <h1>Picture:</h1>
-            <input placeholder="type here" />
-          </label>
-          <button>Submit</button>
+          <button type="submit">Submit</button>
         </form>
+        <a href={`/profile/${tasks[0].personId}`}> Volver Atrás</a>
       </Layout>
     </>
   );
+}
+async function getAllTasksId() {
+  const data = await fetch("http://localhost:3001/tasks");
+  const jsondata = await data.json();
+
+  return jsondata.map((e) => ({
+    params: {
+      id: e.id.toString(),
+    },
+  }));
+}
+export async function getTasks(id) {
+  const data = await fetch("http://localhost:3001/tasks");
+  const jsondata = await data.json();
+  const tasks = jsondata.filter((e) => e.id == id);
+
+  return tasks;
+}
+export async function getStaticPaths() {
+  const tasks = await getAllTasksId();
+
+  return {
+    paths: tasks,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const tasks = await getTasks(params.id);
+
+  return {
+    props: {
+      tasks,
+    },
+  };
+}
+
+export async function putTask(id, payload) {
+  try {
+    const res = await axios.put(`http://localhost:3001/tasks/${id}`, payload);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
